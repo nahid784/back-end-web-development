a77d85d5e9d9df051edce9fdb0cc2880912faa6e
nahid784 <nahidhasantusher784@gmail.com>
2024-12-22 02:00:59 +0600

    if else condtion

---
+++ PHP/if else condition.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>if else condition</title>
+</head>
+<body>
+<?php
+ $username = "abir";
+//  when condition is true
+if($username == "abir"){
+    echo "Login Successfully";
+}
+else{
+    echo "Login error";
+}
+echo "<br>";
+
+//  when condition is false
+if($username == "Nahid"){
+    echo "Login Successfully";
+}
+else echo "Login error";
+?>
+</body>
+</html>
