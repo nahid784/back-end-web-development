b823c5925d8ddae11bedeceed279161bd315ca6c
nahid784 <nahidhasantusher784@gmail.com>
2024-12-22 02:00:44 +0600

    php basic

---
+++ PHP/basic.php
@@ -0,0 +1,38 @@
+<html>
+<head></head>
+<!-- scalars.php -->
+<body>
+<?php
+
+// variable declation
+$name = "nahid";
+$salary = 12000;
+$add = "dhaka";
+
+// for printing
+echo "His name is $name and his salary is $salary. He lives in $add.";
+
+
+// for bolean value (can be true or false)
+$isadmin = true;
+echo "<br>"; // for line break in php
+echo $isadmin;
+
+echo "<br>"; 
+// single quotation string value
+echo 'Hello this is $name and my salary is $salary';
+
+echo "<br>";
+$first_name = "abir";
+$Name = "first_name";
+echo $$Name; //output will be abir for double $$
+echo "<br>";
+
+?>
+
+<!-- dynamic php inside html -->
+<h1> Hello <?php echo $first_name; ?>,  you salary is <?php echo $salary; ?></h1>
+
+</p>
+</body>
+</html>
