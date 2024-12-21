130abd3d36c300fb410c113c4a4d70d7f010385c
nahid784 <nahidhasantusher784@gmail.com>
2024-12-21 23:37:33 +0600

    php file
    this is a practice file of php for my web engineering course at university

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+<head></head>
+<!-- scalars.php -->
+<body>
+<?php
+
+// variable declation
+$name = "nahid";
+$salary = 12000;
+$add = "dhaka";
+
+// for printing
+echo "His name is $name and his salary is $salary. He lives in $add.";
+
+
+// for bolean value (can be true or false)
+$isadmin = true;
+echo "<br>"; // for line break in php
+echo $isadmin;
+
+echo "<br>"; 
+// single quotation string value
+echo 'Hello this is $name and my salary is $salary';
+
+echo "<br>";
+$first_name = "abir";
+$Name = "first_name";
+echo $$Name; //output will be abir for double $$
+
+
+
+?>
+
+</p>
+</body>
+</html>
